b0a6172c5c75ef0f932c6af9a5cadbc0bc98b6c9
Ahmed <belal@fas.harvard.edu>
2010-04-22 00:47:02 -0400

    got registration and login to work, almost have set_preferences working

--- main_site/views.py
+++ main_site/views.py
@@ -1,8 +1,10 @@
 from myrchme.main_site.models import *
+from myrchme.main_site.my_forms import *
+from django.forms.models import inlineformset_factory
 
 #HTTP rendering tools
 from django.shortcuts import render_to_response, get_object_or_404, redirect
-from django.http import HttpResponse, HttpResponseRedirect
+#from django.http import HttpResponse, HttpResponseRedirect
 
 #Django Authentication stuff
 from django.contrib.auth.decorators import login_required
@@ -18,12 +20,14 @@ def index(request):
     index:
     Main page logic.
     """
-    return render_to_response('base/index.html')
+    curr_user = request.user
+
+    return render_to_response('base/index.html', {'curr_user':curr_user})
 
 
-def register(request):
+def register_person(request):
     """
-    register:
+    registerPerson:
     """
     if request.user.id:
         return redirect('/profile.html')
@@ -34,7 +38,7 @@ def register(request):
         form = RegisterForm(request.POST)
         #if from is valid, add the user and log them in
         if form.is_valid():
-                new_user = form.save(request=request)
+                form.save(request=request)
                 user = authenticate(username=form.cleaned_data["username"],
                                     password=form.cleaned_data["password"])
                 login(request, user)
@@ -47,3 +51,33 @@ def register(request):
         form = RegisterForm()
         
     return render_to_response('base/register.html', {'form':form})
+
+
+@login_required
+def set_preferences(request):
+    """
+    set_preferences
+    """
+    curr_user = request.user
+    curr_person = Person.objects.get(username=curr_user.username)
+    preferences = PersPref.objects.filter(user=curr_person)
+
+    PersPrefFormSet = inlineformset_factory(Person, PersPref, max_num=1)
+    pref_form = PersPrefFormSet(instance=curr_person)
+    
+    if request.method == 'POST':
+        #pers_pref = PersPref(user=curr_person)
+        pref_form = PersPrefFormSet(request.POST, instance=curr_person)
+        if pref_form.is_valid():
+            pref_form.save()
+            message = "Preferences successfully saved."
+            return render_to_response('base/preferences.html',
+                {'preferences':preferences, 'pref_form':pref_form,
+                 'message':message})
+        else:
+            pref_form.error = pref_form.errors
+    return render_to_response('base/preferences.html',
+                {'preferences':preferences, 'pref_form':pref_form, 'errors':pref_form.errors})
+
+        
+
--- urls.py
+++ urls.py
@@ -5,7 +5,11 @@ from django.conf.urls.defaults import *
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^$', 'myrchme.main_site.views.index')
+    (r'^$', 'myrchme.main_site.views.index'),
+    (r'^register$', 'myrchme.main_site.views.register_person'),
+    (r'^profile$', 'myrchme.main_site.views.view_profile'),
+    (r'^preferences$', 'myrchme.main_site.views.set_preferences')
+
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
