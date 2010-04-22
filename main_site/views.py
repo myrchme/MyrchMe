d473f2021b722cc85093cdfac928b3dfd5ada514
Ahmed <belal@fas.harvard.edu>
2010-04-21 20:35:10 -0400

    added registration stuff

--- main_site/views.py
+++ main_site/views.py
@@ -15,7 +15,35 @@ from django.contrib.auth import login, authenticate, logout
 
 def index(request):
     """
-    index
+    index:
     Main page logic.
     """
     return render_to_response('base/index.html')
+
+
+def register(request):
+    """
+    register:
+    """
+    if request.user.id:
+        return redirect('/profile.html')
+
+    #handles registration form, registers user
+    if request.method == 'POST':
+        #get form
+        form = RegisterForm(request.POST)
+        #if from is valid, add the user and log them in
+        if form.is_valid():
+                new_user = form.save(request=request)
+                user = authenticate(username=form.cleaned_data["username"],
+                                    password=form.cleaned_data["password"])
+                login(request, user)
+                return redirect('/profile.html')
+        else:
+                form.error = "Form error."
+
+    else:
+        #request is a GET (seeing page for 1st time)
+        form = RegisterForm()
+        
+    return render_to_response('base/register.html', {'form':form})
