604c6d86758c20f1401c68b6c0c12e7c42fb9e53
Ahmed <belal@fas.harvard.edu>
2010-04-24 17:07:50 -0400

    oops, just added the new urls.py

---
+++ main_site/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    (r'^$', 'myrchme.main_site.views.index'),
+    (r'^register$', 'myrchme.main_site.views.register_person'),
+    (r'^register-vendor$', 'myrchme.main_site.views.register_vendor'),
+    (r'^profile$', 'myrchme.main_site.views.view_person_profile'),
+    (r'^store-profile$', 'myrchme.main_site.views.view_my_store_profile'),
+    (r'^preferences$', 'myrchme.main_site.views.set_preferences'),
+    (r'^account$', 'myrchme.main_site.views.change_person_account')
+)
